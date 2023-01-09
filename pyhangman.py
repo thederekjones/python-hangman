4e56004e0ae63001a24776079a8aaa2c6bac81cb
Derek Jones <lab@derekj.dev>
2023-01-08 20:02:50 -0500

    Add 'display' List for and show either '_' or guessed letter

--- pyhangman.py
+++ pyhangman.py
@@ -5,12 +5,23 @@ word_list = ["aardvark", "baboon", "camel"]
 # Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
 
-# Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Please choose a letter. ").lower()
+#Testing code
+print(f'The solution is {chosen_word}.')
 
-# Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+# Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+guess = input("Guess a letter: ").lower()
+
+# Create an empty List called display.
+display = []
+
+# For each letter in the chosen_word, add a "_" to 'display'.
 for letter in chosen_word:
-  if letter == guess:
-    print("Right")
-  else:
-    print("Wrong")
+  display.append("_")
+  
+# Loop through each position in the chosen_word; If the letter at that position
+# matches 'guess' then reveal that letter in the display at that position.
+for index in range(len(chosen_word)):
+  if chosen_word[index] == guess:
+    display[index] = guess
+
+print(display)
