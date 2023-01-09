4c0ae9dc805450bf61351bdc7037ff191e307c02
Derek Jones <lab@derekj.dev>
2023-01-09 03:07:23 -0500

    Add hangman art based on lives remaining

--- pyhangman.py
+++ pyhangman.py
@@ -89,3 +89,18 @@ while end_of_game == False:
     if "_" not in display:
         end_of_game = True
         print("You win.")
+
+    if lives == 6:
+        print(stages[6])
+    elif lives == 5:
+        print(stages[5])
+    elif lives == 4:
+        print(stages[4])
+    elif lives == 3:
+        print(stages[3])
+    elif lives == 2:
+        print(stages[2])
+    elif lives == 1:
+        print(stages[1])
+    elif lives == 0:
+        print(stages[0])
