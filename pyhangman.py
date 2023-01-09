07f04b4030920ab4a9ae03dc19abc68fb9d8c00f
Derek Jones <lab@derekj.dev>
2023-01-09 03:15:55 -0500

    Move hangman art into hangman_art.py and import module

--- pyhangman.py
+++ pyhangman.py
@@ -1,61 +1,5 @@
 import random
-
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+import hangman_art
 
 word_list = ["aardvark", "baboon", "camel"]
 
@@ -91,16 +35,16 @@ while end_of_game == False:
         print("You win.")
 
     if lives == 6:
-        print(stages[6])
+        print(hangman_art.stages[6])
     elif lives == 5:
-        print(stages[5])
+        print(hangman_art.stages[5])
     elif lives == 4:
-        print(stages[4])
+        print(hangman_art.stages[4])
     elif lives == 3:
-        print(stages[3])
+        print(hangman_art.stages[3])
     elif lives == 2:
-        print(stages[2])
+        print(hangman_art.stages[2])
     elif lives == 1:
-        print(stages[1])
+        print(hangman_art.stages[1])
     elif lives == 0:
-        print(stages[0])
+        print(hangman_art.stages[0])
