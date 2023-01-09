cb28781771dd3bf43421e0a011bab513803d0b87
Derek Jones <lab@derekj.dev>
2023-01-09 03:06:24 -0500

    Add lose condition if too many wrong guesses

--- pyhangman.py
+++ pyhangman.py
@@ -1,8 +1,67 @@
 import random
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
+end_of_game = False
+lives = 6
 
 #Testing code
 print(f'The solution is {chosen_word}.')
@@ -10,14 +69,23 @@ print(f'The solution is {chosen_word}.')
 display = []
 
 for letter in chosen_word:
-  display.append("_")
+    display.append("_")
 
-while "_" in display:
-  guess = input("Guess a letter: ").lower()
-  for index in range(len(chosen_word)):
-    if chosen_word[index] == guess:
-      display[index] = guess
-  print(f"{' '.join(display)}")
+while end_of_game == False:
+    guess = input("Guess a letter: ").lower()
 
-if "_" not in display:
-  print("You win.")
+    for index in range(len(chosen_word)):
+        if chosen_word[index] == guess:
+            display[index] = guess
+
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    print(f"{' '.join(display)}")
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
