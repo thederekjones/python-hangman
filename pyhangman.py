43d1ddfaf37724476b618a15e81dd805519640fd
Derek Jones <lab@derekj.dev>
2023-01-09 02:45:34 -0500

    Add while loop to allow multiple guesses

--- pyhangman.py
+++ pyhangman.py
@@ -2,26 +2,22 @@ import random
 
 word_list = ["aardvark", "baboon", "camel"]
 
-# Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
 
 #Testing code
 print(f'The solution is {chosen_word}.')
 
-# Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Guess a letter: ").lower()
-
-# Create an empty List called display.
 display = []
 
-# For each letter in the chosen_word, add a "_" to 'display'.
 for letter in chosen_word:
   display.append("_")
-  
-# Loop through each position in the chosen_word; If the letter at that position
-# matches 'guess' then reveal that letter in the display at that position.
-for index in range(len(chosen_word)):
-  if chosen_word[index] == guess:
-    display[index] = guess
 
-print(display)
+while "_" in display:
+  guess = input("Guess a letter: ").lower()
+  for index in range(len(chosen_word)):
+    if chosen_word[index] == guess:
+      display[index] = guess
+  print(f"{' '.join(display)}")
+
+if "_" not in display:
+  print("You win.")
